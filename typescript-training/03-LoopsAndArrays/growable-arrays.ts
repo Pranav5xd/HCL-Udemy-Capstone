0dfb2b7305cfafdf5777947da7e9403ac539178e
Pranav5xd <49649423+Pranav5xd@users.noreply.github.com>
2022-02-28 13:22:46 -0500

    clear main branch

--- typescript-training/03-LoopsAndArrays/growable-arrays.ts
+++
@@ -1,7 +0,0 @@
-let sportsTwo: string[] = ["Golf", "Cricket", "Football"];
-sportsTwo.push("Baseball");
-sportsTwo.push("Tennis");
-
-for (let tempSport of sportsTwo) {
-    console.log(tempSport);
-}
--- typescript-training/04-Classes/05-modules/Driver.ts
+++
@@ -1,7 +0,0 @@
-
-import{Customer} from "./Customer"
-
-let myCustomer = new Customer("Pranav", "Sharma");
-
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
